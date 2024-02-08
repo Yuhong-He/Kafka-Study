3014989663cc42e222c3c0eae57db840d02c7192
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2024-02-08 09:51:07 +0000

    update

--- SpringBoot/src/main/java/com/example/springboot/consumer/MyKafkaConsumer.java
+++ SpringBoot/src/main/java/com/example/springboot/consumer/MyKafkaConsumer.java
@@ -1,2 +1,31 @@
-package com.example.springboot.consumer;public class MyKafkaConsumer {
+package com.example.springboot.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyKafkaConsumer {
+    @KafkaListener(topics = "my-replicated-topic", groupId = "MyGroup1")
+    public void listenGroup(ConsumerRecord<String, String> record, Acknowledgment ack) {
+        String value = record.value();
+        System.out.println(value);
+        System.out.println(record);
+        ack.acknowledge();
+    }
+
+    @KafkaListener(groupId = "testGroup", topicPartitions = {
+            @TopicPartition(topic = "topic1", partitions = {"0", "1"}),
+            @TopicPartition(topic = "topic2", partitions = "0",
+                partitionOffsets = @PartitionOffset(partition = "1", initialOffset = "100"))
+    }, concurrency = "3")
+    public void listenGroupPro(ConsumerRecord<String, String> record, Acknowledgment ack) {
+        String value = record.value();
+        System.out.println(value);
+        System.out.println(record);
+        ack.acknowledge();
+    }
 }
--- SpringBoot/src/main/java/com/example/springboot/controller/MyKafkaController.java
+++ SpringBoot/src/main/java/com/example/springboot/controller/MyKafkaController.java
@@ -1,2 +1,22 @@
-package com.example.springboot.controller;public class MyKafkaController {
+package com.example.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/msg")
+public class MyKafkaController {
+
+    private final static String TOPIC_NAME = "my-replicated-topic";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @RequestMapping("/send")
+    public String sendMsg() {
+        kafkaTemplate.send(TOPIC_NAME, 0, "key", "This is a message!");
+        return "Send success.";
+    }
 }
